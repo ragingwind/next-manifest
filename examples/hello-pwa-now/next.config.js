bd573ccaf7addb5bfa2b47411deff24b3938db25
Jimmy Moon <ragingwind@gmail.com>
2019-08-12 08:42:24 +0900

    Update hello-pwa-now

--- examples/hello-pwa-now/next.config.js
+++ examples/hello-pwa-now/next.config.js
@@ -1,4 +1,3 @@
-const { resolve } = require('path');
 const withManifest = require('next-manifest');
 
 module.exports = withManifest({
--- examples/hello-pwa-now/pages/_document.js
+++ examples/hello-pwa-now/pages/_document.js
@@ -1,26 +1,28 @@
-import Document, { Head, Main, NextScript } from 'next/document';
-import flush from 'styled-jsx/server';
-import Manifest from 'next-manifest';
+import Document, {Head, Main, NextScript} from 'next/document'
+import flush from 'styled-jsx/server'
+import {Manifest} from 'next-manifest';
+
+console.log('Manifest', Manifest);
 
 export default class extends Document {
-  static getInitialProps({ renderPage }) {
+  static getInitialProps({renderPage}) {
     return {
       ...renderPage(),
       styles: flush()
-    };
+    }
   }
 
   render() {
     return (
       <html lang="en">
         <Head>
-          <Manifest href={'/static/manifest.json'} themeColor={'#F0F0F0'} />
+          <Manifest href="/static/manifest.json" themeColor="#F0F0F0"/>
         </Head>
         <body>
           <Main />
           <NextScript />
         </body>
       </html>
-    );
+    )
   }
 }
